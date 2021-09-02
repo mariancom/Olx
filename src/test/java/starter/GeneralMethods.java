c7792c9c4bbdf140f1d5bf5151b53040b4cefdb2
marian <marian>
2021-09-02 12:14:27 +0300

    Added shadow root click

--- src/test/java/starter/GeneralMethods.java
+++ src/test/java/starter/GeneralMethods.java
@@ -1,40 +1,50 @@
 package starter;
 
 import net.serenitybdd.core.pages.WebElementFacade;
-import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.ElementNotInteractableException;
-import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.*;
 
 import java.util.NoSuchElementException;
 
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
+
 public class GeneralMethods {
 
-    public static void clickBtn(WebElementFacade webElementFacade, String buttonName){
+    public static void clickBtn(WebElementFacade webElementFacade, String buttonName) {
 
         try {
             System.out.println("Clicking " + buttonName);
             webElementFacade.waitUntilClickable().click();
-        } catch (NoSuchElementException nse){
+        } catch (NoSuchElementException nse) {
             System.out.println("Button " + buttonName + " not present");
-        } catch (ElementClickInterceptedException ecie){
+        } catch (ElementClickInterceptedException ecie) {
             System.out.println("Button " + buttonName + "intercepted exception");
-        } catch (StaleElementReferenceException sere){
+        } catch (StaleElementReferenceException sere) {
             System.out.println("Button " + buttonName + "stale element exception");
         }
     }
 
-    public static void sendKeys(WebElementFacade webElementFacade, String keys, String elementName){
+    public static void sendKeys(WebElementFacade webElementFacade, String keys, String elementName) {
 
         try {
             webElementFacade.waitUntilClickable();
             System.out.println("Sending keys " + keys + " on " + elementName);
             webElementFacade.sendKeys(keys);
-        } catch (NoSuchElementException nse){
+        } catch (NoSuchElementException nse) {
             System.out.println("Field " + elementName + " not present");
-        } catch (ElementNotInteractableException enie){
+        } catch (ElementNotInteractableException enie) {
             System.out.println("Field " + elementName + " is not interactable");
         }
 
     }
 
+    public static void shadowClick(String locator, String argument, String elementName) {
+        JavascriptExecutor jse = (JavascriptExecutor) getDriver();
+        WebElement webElement = (WebElement) jse.executeScript(locator);
+        System.out.println("Clicking on shadowRoot element " + elementName);
+        ((JavascriptExecutor) getDriver()).executeScript(argument, webElement);
+    }
+
+
+
 }
+
